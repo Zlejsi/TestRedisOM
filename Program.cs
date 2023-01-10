b65e2eb88912ad8b275aabbf4682500345371d41
u04537 <michal.zlacky@trz.cz>
2023-01-10 14:39:56 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace ConsoleApp1
+{
+
+
+    public class IntID
+    {
+        public int Id { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntID id = new IntID();
+            Type type = id.GetType();
+
+            PropertyInfo info = type.GetProperties().First(x => x.Name == "Id");
+            info.SetValue(id,(object)"2");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
