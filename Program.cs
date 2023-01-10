826c90e84f2152b928677607f0e71fb69fc1d083
u04537 <michal.zlacky@trz.cz>
2023-01-10 14:58:21 +0100

    Exception at Insert

--- Program.cs
+++ Program.cs
@@ -1,26 +1,55 @@
 ﻿using System;
+using System.Globalization;
 using System.Linq;
 using System.Reflection;
+using Redis.OM;
+using Redis.OM.Contracts;
+using Redis.OM.Modeling;
+using Redis.OM.Searching;
+using StackExchange.Redis;
 
 namespace ConsoleApp1
 {
 
+    public class StaticIncrementStrategy : IIdGenerationStrategy
+    {
+        private readonly IRedisConnection _db;
+        private readonly Type _type;
 
+        public StaticIncrementStrategy(IRedisConnection db, Type type)
+        {
+            _db = db;
+            _type = type;
+
+        }
+        public string GenerateId()
+        {
+            return _db.Execute("INCR", new string[] { _type.Name + ":ID" }).ToString(CultureInfo.InvariantCulture);
+        }
+    }
+
+    [Document(StorageType = StorageType.Hash, IdGenerationStrategyName = "MyIncrementStrategy", Prefixes = new string[] { nameof(IntID) })]
     public class IntID
     {
-        public int Id { get; set; }
+        [RedisIdField] public int Id { get; set; }
+        public string Name { get; set; }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            IntID id = new IntID();
-            Type type = id.GetType();
+            var options = new ConfigurationOptions();
+            options.EndPoints.Add("", 0); //provide host and port
+            var provider = new RedisConnectionProvider(options);
+            var _items = provider.RedisCollection<IntID>();
 
-            PropertyInfo info = type.GetProperties().First(x => x.Name == "Id");
-            info.SetValue(id,(object)"2");
-            Console.WriteLine("Hello World!");
+            DocumentAttribute.RegisterIdGenerationStrategy("MyIncrementStrategy", new StaticIncrementStrategy(provider.Connection, typeof(IntID)));
+
+            IntID id = new() { Name = "Test"};
+            _items.Insert(id);
+
+            Console.WriteLine("");
         }
     }
 }
